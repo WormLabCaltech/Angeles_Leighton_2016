3b0dac75e08249df63283c3b37a6c66a5eb556e8
David Angeles <dangeles@caltech.edu>
2017-02-28 23:36:42 -0800

    added epistasis plot analysis
    yay! :D makes everything beautiful and smart and gorgeous and solves a
    lot of problems :)

--- rdocs/aging_rna_seq_analysis.R
+++ rdocs/aging_rna_seq_analysis.R
@@ -1,6 +1,6 @@
-#source("http://bioconductor.org/biocLite.R")
-#biocLite("rhdf5")
-#biocLite("biomaRt")
+source("http://bioconductor.org/biocLite.R")
+biocLite("rhdf5")
+biocLite("biomaRt")
 
 setwd('~/WormFiles/agingRNAseq/rdocs')
 #gene info for sleuth
@@ -9,7 +9,6 @@ t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id",
                                      "external_gene_name"), mart = mart)
 t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id,
                      ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
-
 
 #install.packages("devtools")
 
@@ -38,17 +37,12 @@ so <- sleuth_prep(s2c, ~ genotype+age+genotype*age, target_mapping= t2g)
 
 #fit the models
 so <- sleuth_fit(so,~ genotype + age + genotype*age, fit_name = 'interaction')
-#so <- sleuth_fit(so,~ genotype, fit_name = 'null_genotype')
-#so <- sleuth_fit(so,~ age, fit_name = 'null_age')
 so <- sleuth_fit(so,~ genotype + age, fit_name = 'full')
 
 #Wald test implementations
-#no interactions
-#so <- sleuth_wt(so, which_beta = '(Intercept)', which_model = 'full')
 so <- sleuth_wt(so, which_beta = 'ageold', which_model = 'full')
 so <- sleuth_wt(so, which_beta = 'genotypemt', which_model = 'full')
 #model with interaction
-#so <- sleuth_wt(so, which_beta = '(Intercept)', which_model= 'interaction')
 so <- sleuth_wt(so, which_beta = 'ageold', which_model = 'interaction')
 so <- sleuth_wt(so, which_beta = 'genotypemt', which_model = 'interaction')
 so <- sleuth_wt(so, which_beta = 'genotypemt:ageold', which_model = 'interaction')
@@ -58,22 +52,15 @@ so <- sleuth_lrt(so, 'full', 'interaction')
 #if you want to look at shiny
 sleuth_live(so)
 
-
-#following line is to publish...
-#saveRDS(so, file = '~/shiny/sleuth/aging_fog2_AngelesAndLeighton_2016.rds')
-
 #write results to tables
 results_table <- sleuth_results(so, 'ageold','interaction', test_type= 'wt')
-write.csv(results_table, "~/WormFiles/agingRNAseq/input/agebeta_wt.csv")
+write.csv(results_table, "~/WormFiles/agingRNAseq/input/si2_aging_analysis.csv")
 
 results_table <- sleuth_results(so, 'genotypemt','interaction', test_type= 'wt')
-write.csv(results_table, "~/WormFiles/agingRNAseq/input/genotypebeta_wt.csv")
+write.csv(results_table, "~/WormFiles/agingRNAseq/input/si3_genotype_analysis.csv")
 
 results_table <- sleuth_results(so, 'genotypemt:ageold','interaction', test_type= 'wt')
-write.csv(results_table, "~/WormFiles/agingRNAseq/input/genotypecrossagebeta_wt.csv")
-
-results_table <- sleuth_results(so, '(Intercept)','interaction', test_type= 'wt')
-write.csv(results_table, "~/WormFiles/agingRNAseq/input/intercept_wt.csv")
+write.csv(results_table, "~/WormFiles/agingRNAseq/input/si4_interaction_analysis.csv")
 
 results_table <- sleuth_results(so, 'full:interaction', test_type='lrt')
 write.csv(results_table, "~/WormFiles/agingRNAseq/input/lrt.csv")
